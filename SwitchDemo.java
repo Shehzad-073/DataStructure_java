e5ce5ac72ab8f09c4d096d6625621720ecd2be33
Muhammad Shehzad <m10351905@gmail.com>
2026-02-26 21:12:16 +0500

    switch Demo to clear java concept

---
+++ SwitchDemo.java
@@ -0,0 +1,28 @@
+package JavaConcept;
+
+public class SwitchDemo {
+    static void main() {
+        String day = "Monday";
+        switch (day){
+            case "Monday":
+                System.out.println("Today is monday");
+                break;
+
+            case "Tuesday":
+                System.out.println("Today is Tuesday");
+                break;
+
+            case "Wednesday":
+                System.out.println("Today is Wednesday");
+                break;
+
+            case "Thursday":
+                System.out.println("Today is Thursday");
+                break;
+
+            case "Friday":
+                System.out.println("Today is Friday");
+                break;
+        }
+    }
+}
